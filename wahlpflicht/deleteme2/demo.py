6e178067ab0f05ef3715c1a155f8bf433491f281
Nico Meyer <nico.meyer@neuland-bfi.de>
2024-09-13 10:20:41 +0200

    SAVE

---
+++ wahlpflicht/deleteme2/demo.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+print("Hello World")
+
+text = 5
+text2 = f"This is {text:5d} a string"
+print(text2)
+
+someList = [1, 2, 3, 4]
+print(someList)
+
+def func():
+    print("Hello World")
